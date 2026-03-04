65adaa4668bb7aec3fc79159e5cda06bcae677cd
Jaydeep097 <jaydeepyaduvanshi09@gmail.com>
2026-03-04 19:42:29 +0530

    .

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,7 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {
+    extend:{
       fontFamily: {
         satoshi: [ "satoshi","sans-serif" ],
         inter: ["inter", "sans-serif"],
